c706e85e10be2f8bb742203ebefe9a793cf58c21
Jenn Angell <jwpierce14@gmail.com>
2025-06-15 19:47:54 -0700

    finished adding in pots

--- src/data/pottery.js
+++ src/data/pottery.js
@@ -1,117 +1,108 @@
-import angellJenn01 from '../images/images_pots/AngellJenn_01.jpg';
-import angellJenn02 from '../images/images_pots/AngellJenn_02.jpg';
-import angell02 from '../images/images_pots/Angell_02.jpg';
-import angell04 from '../images/images_pots/Angell_04.jpg';
-import angellJar from '../images/images_pots/Angell_jar.jpg';
-import angellJenn1 from '../images/images_pots/Angell_Jenn_1.jpg';
-import angellJenn2 from '../images/images_pots/Angell_Jenn_2.jpg';
-import angellJenn3 from '../images/images_pots/Angell_Jenn_3.jpg';
-import dsc0032 from '../images/images_pots/DSC_0032.jpg';
-import dsc0103 from '../images/images_pots/DSC_0103.jpg';
-import dsc0104 from '../images/images_pots/DSC_0104.jpg';
-import dsc0106 from '../images/images_pots/DSC_0106.jpg';
-import ja6 from '../images/images_pots/ja6.jpg';
-import brainMug2 from '../images/images_pots/JENN ANGELL_Brain Mug (2nd view)_stoneware, terra sigilatta, glaze, underglaze_wheel thrown, pulled handle_soda fired to cone 10_3_ x 3_ x 4_.jpg';
-import brainMug from '../images/images_pots/JENN ANGELL_Brain Mug_2018_stoneware, terra sigilatta, glaze, underglaze_wheel thrown, pulled handle_soda fired to cone 10_3_ x 3_ x 4_.jpg';
-import jennAngell01 from '../images/images_pots/JennAngell_01.jpg';
+import img_02 from '../images/images_pots/IMG_002.jpg';
+import img_02_small from '../images/images_pots/IMG_002_small.jpg';
+import img_03 from '../images/images_pots/IMG_003.jpg';
+import img_03_small from '../images/images_pots/IMG_003_small.jpg';
+import img_04 from '../images/images_pots/IMG_004.jpg';
+import img_04_small from '../images/images_pots/IMG_004_small.jpg';
+import img_05 from '../images/images_pots/IMG_005.jpg';
+import img_05_small from '../images/images_pots/IMG_005_small.jpg';
+import img_06 from '../images/images_pots/IMG_006.jpg';
+import img_06_small from '../images/images_pots/IMG_006_small.jpg';
+import img_07 from '../images/images_pots/IMG_007.jpg';
+import img_07_small from '../images/images_pots/IMG_007_small.jpg';
+import img_08 from '../images/images_pots/IMG_008.jpg';
+import img_08_small from '../images/images_pots/IMG_008_small.jpg';
+import img_09 from '../images/images_pots/IMG_009.jpg';
+import img_09_small from '../images/images_pots/IMG_009_small.jpg';
+import img_10 from '../images/images_pots/IMG_010.jpg';
+import img_10_small from '../images/images_pots/IMG_010_small.jpg';
+import img_11 from '../images/images_pots/IMG_011.jpg';
+import img_11_small from '../images/images_pots/IMG_011_small.jpg';
+import img_12 from '../images/images_pots/IMG_012.jpg';
+import img_12_small from '../images/images_pots/IMG_012_small.jpg';
+import img_13 from '../images/images_pots/IMG_013.jpg';
+import img_13_small from '../images/images_pots/IMG_013_small.jpg';
+import img_14 from '../images/images_pots/IMG_014.jpg';
+import img_14_small from '../images/images_pots/IMG_014_small.jpg';
+import img_015 from '../images/images_pots/IMG_015.jpg';
+import img_015_small from '../images/images_pots/IMG_015_small.jpg';
+import img_016 from '../images/images_pots/IMG_016.jpg';
+import img_016_small from '../images/images_pots/IMG_016_small.jpg';
+import img_017 from '../images/images_pots/IMG_017.jpg';
+import img_017_small from '../images/images_pots/IMG_017_small.jpg';
+import img_018 from '../images/images_pots/IMG_018.jpg';
+import img_018_small from '../images/images_pots/IMG_018_small.jpg';
+import img_019 from '../images/images_pots/IMG_019.jpg';
+import img_019_small from '../images/images_pots/IMG_019_small.jpg';
+import img_020 from '../images/images_pots/IMG_020.jpg';
+import img_020_small from '../images/images_pots/IMG_020_small.jpg';
+import img_021 from '../images/images_pots/IMG_021.jpg';
+import img_021_small from '../images/images_pots/IMG_021_small.jpg';
+import img_022 from '../images/images_pots/IMG_022.jpg';
+import img_022_small from '../images/images_pots/IMG_022_small.jpg';
+import img_023 from '../images/images_pots/IMG_023.jpg';
+import img_023_small from '../images/images_pots/IMG_023_small.jpg';
+import img_024 from '../images/images_pots/IMG_024.jpg';
+import img_024_small from '../images/images_pots/IMG_024_small.jpg';
+import img_26 from '../images/images_pots/IMG_26.jpg';
+import img_27 from '../images/images_pots/IMG_27.jpg';
+import img_28 from '../images/images_pots/IMG_28.jpg';
+import img_29 from '../images/images_pots/IMG_29.jpg';
+import img_30 from '../images/images_pots/IMG_30.jpg';
+import img_31 from '../images/images_pots/IMG_31.jpg';
+import img_32 from '../images/images_pots/IMG_32.jpg';
+import img_33 from '../images/images_pots/IMG_33.jpg';
+import img_34 from '../images/images_pots/IMG_34.jpg';
+import img_35 from '../images/images_pots/IMG_35.jpg';
+import img_36 from '../images/images_pots/IMG_36.jpg';
+import img_37 from '../images/images_pots/IMG_37.jpg';
+import img_38 from '../images/images_pots/IMG_38.jpg';
+import img_39 from '../images/images_pots/IMG_39.jpg';
+import img_40 from '../images/images_pots/IMG_40.jpg';
+import img_41 from '../images/images_pots/IMG_41.jpg';
 
 const pottery = [
-    {
-        id: 1,
-        title: "TBD",
-        imageUrl: brainMug,
-        description: "TBD",
-    },
-    {
-        id: 2,
-        title: "TBD",
-        imageUrl: brainMug2,
-        description: "TBD",
-    },
-    {
-        id: 3,
-        title: "TBD",
-        imageUrl: angellJenn01,
-        description: "TBD",
-    },
-    {
-        id: 4,
-        title: "TBD",
-        imageUrl: angellJenn02,
-        description: "TBD",
-    },
-    {
-        id: 5,
-        title: "TBD",
-        imageUrl: angellJar,
-        description: "TBD",
-    },
-    {
-        id: 6,
-        title: "TBD",
-        imageUrl: angellJenn1,
-        description: "TBD",
-    },
-    {
-        id: 7,
-        title: "TBD",
-        imageUrl: angellJenn2,
-        description: "TBD",
-    },
-    {
-        id: 8,
-        title: "TBD",
-        imageUrl: angellJenn3,
-        description: "TBD",
-    },
-    {
-        id: 9,
-        title: "TBD",
-        imageUrl: dsc0032,
-        description: "TBD",
-    },
-    {
-        id: 10,
-        title: "TBD",
-        imageUrl: dsc0103,
-        description: "TBD",
-    },
-    {
-        id: 11,
-        title: "TBD",
-        imageUrl: dsc0104,
-        description: "TBD",
-    },
-    {
-        id: 12,
-        title: "TBD",
-        imageUrl: dsc0106,
-        description: "TBD",
-    },
-    {
-        id: 13,
-        title: "TBD",
-        imageUrl: ja6,
-        description: "TBD",
-    },
-    {
-        id: 14,
-        title: "TBD",
-        imageUrl: angell02,
-        description: "TBD",
-    },
-    {
-        id: 15,
-        title: "TBD",
-        imageUrl: angell04,
-        description: "TBD",
-    },
-    {
-        id: 16,
-        title: "TBD",
-        imageUrl: jennAngell01,
-        description: "TBD",
-    }
+    
+    { id: 2, imageUrl: img_02, previewUrl: img_02_small },
+    { id: 3, imageUrl: img_03, previewUrl: img_03_small },
+    { id: 4, imageUrl: img_04, previewUrl: img_04_small },
+    { id: 5, imageUrl: img_05, previewUrl: img_05_small },
+    { id: 6, imageUrl: img_06, previewUrl: img_06_small },
+    { id: 7, imageUrl: img_07, previewUrl: img_07_small },
+    { id: 8, imageUrl: img_08, previewUrl: img_08_small },
+    { id: 9, imageUrl: img_09, previewUrl: img_09_small },
+    { id: 10, imageUrl: img_10, previewUrl: img_10_small },
+    { id: 11, imageUrl: img_11, previewUrl: img_11_small },
+    { id: 12, imageUrl: img_12, previewUrl: img_12_small },
+    { id: 13, imageUrl: img_13, previewUrl: img_13_small },
+    { id: 14, imageUrl: img_14, previewUrl: img_14_small },
+    { id: 15, imageUrl: img_015, previewUrl: img_015_small },
+    { id: 16, imageUrl: img_016, previewUrl: img_016_small },
+    { id: 17, imageUrl: img_017, previewUrl: img_017_small },
+    { id: 18, imageUrl: img_018, previewUrl: img_018_small },
+    { id: 19, imageUrl: img_019, previewUrl: img_019_small },
+    { id: 20, imageUrl: img_020, previewUrl: img_020_small },
+    { id: 21, imageUrl: img_021, previewUrl: img_021_small },
+    { id: 22, imageUrl: img_022, previewUrl: img_022_small },
+    { id: 23, imageUrl: img_023, previewUrl: img_023_small },
+    { id: 24, imageUrl: img_024, previewUrl: img_024_small },
+    
+    { id: 26, imageUrl: img_26 },
+    { id: 27, imageUrl: img_27 },
+    { id: 28, imageUrl: img_28 },
+    { id: 29, imageUrl: img_29 },
+    { id: 30, imageUrl: img_30 },
+    { id: 31, imageUrl: img_31 },
+    { id: 32, imageUrl: img_32 },
+    { id: 33, imageUrl: img_33 },
+    { id: 34, imageUrl: img_34 },
+    { id: 35, imageUrl: img_35 },
+    { id: 36, imageUrl: img_36 },
+    { id: 37, imageUrl: img_37 },
+    { id: 38, imageUrl: img_38 },
+    { id: 39, imageUrl: img_39 },
+    { id: 40, imageUrl: img_40 },
+    { id: 41, imageUrl: img_41 },
 ];
 
 export default pottery;
